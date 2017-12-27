22de3b928670ecbf1a4e1557053534498ce2975c
Silky Ng <32797002+silkster87@users.noreply.github.com>
2017-12-27 10:34:39 +0000

    Done TODO 1-6

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,17 +1,34 @@
 package com.example.android.waitlist.data;
 
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper{
 
+    
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
-
+    public static final String DATABASE_NAME = "waitlist.db";
+    
+    
     // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
-
+    public static final int DATABASE_VERSION = 1;
+    
     // TODO (4) Create a Constructor that takes a context and calls the parent constructor
-
+    public WaitlistDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
     // TODO (5) Override the onCreate method
-
-        // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase){
+    // TODO (6) Inside, create an String query called SQL_CREATE_WAITLIST_TABLE that will create the table
+        final String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " +
+            WaitlistEntry.TABLE_NAME + "( " +
+            WaitlistEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+            WaitlistEntry.COLUMN_GUEST_NAME + "TEXT NOT NULL," +
+            WaitlistEntry.COLUMN_PARTY_SIZE + "INTEGER NOT NULL, " + 
+            WaitlistEntry.COLUMN_TIMESTAMP + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+            + " );";
+    }
+        
 
         // TODO (7) Execute the query by calling execSQL on sqLiteDatabase and pass the string query SQL_CREATE_WAITLIST_TABLE
 
